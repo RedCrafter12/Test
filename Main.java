5f0d6d361943f3d793fc1e66b83a6c69f36c10eb
RedCrafter12 <pyahmadi2@gmail.com>
2025-10-02 15:13:12 +0200

    Main.java erstellen

---
+++ Main.java
@@ -0,0 +1,11 @@
+package net.suchsel.test;
+
+import net.suchsel.lib.Field;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
